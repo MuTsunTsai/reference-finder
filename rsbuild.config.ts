b474944d22b951c09d054222bc6b6d19a7d25f5c
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2024-05-17 15:09:49 +0800

    Optimize build

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,16 @@
+import { glob } from "glob";
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginCheckSyntax } from "@rsbuild/plugin-check-syntax";
 import { GenerateSW } from "workbox-webpack-plugin";
-import purgeCss from "@fullhuman/postcss-purgecss";
+import { PurgeCSSPlugin } from "purgecss-webpack-plugin";
 
 import * as pkg from "./package.json";
 
 const isProduction = process.env.NODE_ENV === "production";
+
+// This is a hack to make purgecss-webpack-plugin working with rspack
+(Array.prototype as unknown as Set<unknown>).has = Array.prototype.includes;
 
 export default defineConfig({
 	dev: {
@@ -71,6 +75,11 @@ export default defineConfig({
 		legalComments: "none",
 		assetPrefix: "/reference-finder/",
 		polyfill: "off",
+		minify: {
+			htmlOptions: {
+				removeComments: true,
+			},
+		},
 		distPath: {
 			root: "docs",
 		},
@@ -81,30 +90,30 @@ export default defineConfig({
 			ecmaVersion: 2018,
 		}),
 	],
-	tools: !isProduction ? undefined : {
-		postcss: {
-			postcssOptions: {
-				plugins: [
-					purgeCss({
-						content: ["./src/app/**/*.tsx", "./src/public/index.html"],
-						safelist: {
-							standard: [
-								/backdrop/,
-								/bs-theme/,
-								/^--bs-btn-disabled/,
-								/^--bs-tooltip/,
-							],
-							greedy: [
-								/tooltip/,
-								/creasePattern|boundary/,
-								/(point|line|arc|label|target)-/,
-							],
-						},
-					}),
-				],
-			},
-		},
-		rspack: (_, { appendPlugins }) => {
+	tools: {
+		rspack: (_, { appendPlugins, isDev }) => {
+			if(isDev) return;
+
+			// Using postcss-purgecss is an alternative,
+			// but that will result in chunks being processed separately.
+			appendPlugins(new PurgeCSSPlugin({
+				paths: glob.sync(["./src/app/**/*.tsx", "./src/public/index.html"]),
+				variables: true,
+				safelist: {
+					standard: [
+						/backdrop/,
+						/bs-theme/,
+						/tooltip/,
+						/modal-static/,
+					],
+					greedy: [
+						/creasePattern|boundary/,
+						/(point|line|arc|label|target)-/,
+					],
+				},
+				blocklist: [],
+			}));
+
 			appendPlugins(new GenerateSW({
 				clientsClaim: true,
 				skipWaiting: true,
