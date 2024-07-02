5b8530c9882f3db334126a984c320db1ebd95810
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2024-07-02 17:19:54 +0800

    Update build tools

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -11,9 +11,6 @@ import * as pkg from "./package.json";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-// This is a hack to make purgecss-webpack-plugin working with rspack
-(Array.prototype as unknown as Set<unknown>).has = Array.prototype.includes;
-
 export default defineConfig({
 	dev: {
 		progressBar: true,
