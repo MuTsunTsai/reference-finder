5d5056f2eba5c73c54b6b1bb86b6272579a6eb69
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2024-12-10 14:50:42 +0800

    Fix syncVer script

--- gulpfile.js
+++ gulpfile.js
@@ -18,14 +18,14 @@ gulp.task("syncVer", async () => {
 	return gulp.src("src/core/RFVersion.h")
 		.pipe(through2(content => {
 			const newContent = content
-				.replace(/(?<=VERSION_MAJOR )\d+/, match[0])
-				.replace(/(?<=VERSION_MINOR )\d+/, match[1])
-				.replace(/(?<=VERSION_BUGFIX )\d+/, match[2]);
+				.replace(/(?<=VERSION_MAJOR = )\d+/, match[0])
+				.replace(/(?<=VERSION_MINOR = )\d+/, match[1])
+				.replace(/(?<=VERSION_BUGFIX = )\d+/, match[2]);
 			if(newContent === content) return null;
 
 			const now = new Date();
 			const today = now.getFullYear() + pad(now.getMonth() + 1) + pad(now.getDate());
-			return newContent.replace(/(?<=VERSION_BUILD )\d+/, today);
+			return newContent.replace(/(?<=VERSION_BUILD = )\d+/, today);
 		}))
 		.pipe(gulp.dest("src/core"));
 });
